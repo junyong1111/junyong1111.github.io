352638fbc1e579c209d2b3acfb17175b3cb1126a
Junyong <jypark93@kookmin.ac.kr>
2022-03-24 18:01:21 +0900

    bronze_4

--- _posts/test.c
+++ _posts/test.c
@@ -2,8 +2,12 @@
 #include <stdlib.h>
 
 int main(){
-    printf("hello world");
+  int n ;
+  scanf("%d", &n);
+  if(n%4==0 && (n%100!=0 || n%400 ==0)){
+      printf("%d\n",1);
+  }
+  else printf("%d\n",0);
 
-    return 0;
+  return 0;
 }
-
