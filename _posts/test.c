52a5be99007eb9296679495eafd91e9b394a7298
Junyong <jypark93@kookmin.ac.kr>
2022-03-27 20:17:55 +0900

    add:bronze2

--- _posts/test.c
+++ _posts/test.c
@@ -1,32 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <stdbool.h>
+#include <string.h>
 
 int main(){
-    int N = 10;
-    int *arr = malloc(N);
-    bool check = false;
-    int cnt = 1;
-    for(int i=0; i<N; i++){
-        scanf("%d",&arr[i]);
-        arr[i] = arr[i]%42;
+    char str[101];
+    int alpha[26];
+    int check = 0;
+    for(int i=0; i<26; i++){
+        alpha[i] = -1;
     }
-    int ch = arr[0];
-    for(int i=1; i<N; i++){
-        if(ch == arr[i] && check == false){check = true; cnt ++;}
-        else{
-
+    int i = 0;
+    int temp = 0;
+    scanf("%s", str);
+    int len = strlen(str);
+    while(len--){
+        temp = str[i] - 97;
+        if(alpha[temp] == -1){
+            alpha[temp] =i;
+            i++;
+        } // 처음 나온 알파벳이면 idx 저장
+        else{ //중복 처리
+            i++;
+            continue;
         }
+    } //while
+    for(int i=0; i<26; i++){
+        printf("%d ", alpha[i]);
     }
-
-    for(int i=0; i<N; i++){
-        printf("%d ",arr[i]);
-    }printf("\n");
-
-
-
-
-    free(arr);
+    printf("\n");
     return 0;
 }
