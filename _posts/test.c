68a8cf7e7c79abf02985847f508f9669ae596d77
Junyong <jypark93@kookmin.ac.kr>
2022-03-28 20:19:14 +0900

    add:b_1

--- _posts/test.c
+++ _posts/test.c
@@ -1,33 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
+#include <stdbool.h>
 
-int main(){
-    char str[101];
-    int alpha[26];
-    int check = 0;
-    for(int i=0; i<26; i++){
-        alpha[i] = -1;
-    }
-    int i = 0;
+void sort(int arr[], int N){
     int temp = 0;
-    scanf("%s", str);
-    int len = strlen(str);
-    while(len--){
-        temp = str[i] - 97;
-        if(alpha[temp] == -1){
-            alpha[temp] =i;
-            i++;
-        } // 처음 나온 알파벳이면 idx 저장
-        else{ //중복 처리
-            i++;
-            continue;
-        }
-    } //while
-    for(int i=0; i<26; i++){
-        printf("%d ", alpha[i]);
+    for(int i=0; i<N; i++){
+       for(int j= i+1; j<N; j++){
+           if(arr[i]>arr[j]){
+               temp = arr[j];
+               arr[j] = arr[i];
+               arr[i] = temp;
+           }
+           else continue;
+       }
+    }//for_i
+    for(int i=0; i<N; i++){
+        printf("%d ", arr[i]);
     }
     printf("\n");
+}
+
+int main(void){
+    int N;
+    scanf("%d",&N);
+    int arr[N];
+    for(int i=0; i<N; i++){
+        scanf("%d", &arr[i]);
+    }
+    sort(arr,N);
+
+
+
     return 0;
 }
